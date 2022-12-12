7bc327f87c98ebbddcfc839f77b242b60f2275e9
dipakshelke23 <dipakshelke2323@gmail.com>
2022-12-12 16:39:49 +0530

    Encapsulation getter and setter method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main (String args[]){
+        RWOnly r1= new RWOnly();
+        r1.setName("RAM");
+        System.out.println(r1.getName());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name;
+    public void setName(String Nav){
+        Name=Nav;
+    }
+    public String getName(){
+      return Name;
+    }
+
+
 }
+
